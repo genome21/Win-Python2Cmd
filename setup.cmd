7de9dc9ad0a67a4848da96ac44864ab82c2d9995
genome21 <thecainvestors@gmail.com>
2016-01-28 14:06:47 -0600

    Added Version Checking to check for functionality

--- setup.cmd
+++ setup.cmd
@@ -1,4 +1,23 @@
 @echo off
-ren python.genome21 python.cmd
-copy python.cmd C:\Windows
-ren python.cmd python.genome21
+REM python --version > version.txt
+SET /a ERL=%ERRORLEVEL%
+GOTO compatibility
+
+:errors
+ECHO Python version not compatible.
+GOTO EOF
+
+:compatibility
+FIND "Python 3.5" version.txt
+IF ERRORLEVEL == 1 (
+	GOTO errors
+) ELSE (
+	GOTO runcode
+)
+
+:runcode
+REN python.genome21 python.cmd
+COPY python.cmd C:\Windows
+REN python.cmd python.genome21
+
+:EOF

49434a560278dc5bb4f321ff7792881e36395914
genome21 <thecainvestors@gmail.com>
2016-01-29 22:44:15 -0600

    Capability added and issue resolved
    Added capability for use with Python 2.7.
    Resolved an issue where the program was relying on itself to be
    installed to version check Python.

--- setup.cmd
+++ setup.cmd
@@ -1,23 +1,44 @@
 @echo off
-REM python --version > version.txt
+%USERPROFILE%\AppData\Local\Programs\Python\Python35-32\python.exe --version > version.txt
 SET /a ERL=%ERRORLEVEL%
-GOTO compatibility
+GOTO compatibility3
 
 :errors
 ECHO Python version not compatible.
 GOTO EOF
 
-:compatibility
+:compatibility3
 FIND "Python 3.5" version.txt
+GOTO ELO1
+
+:compatibility2
+FIND "Python 2.7" version.txt
+GOTO ELO2
+
+:ELO1
+IF ERRORLEVEL == 1 (
+	GOTO compatibility2
+) ELSE (
+	GOTO runcode35
+)
+
+:ELO2
 IF ERRORLEVEL == 1 (
 	GOTO errors
 ) ELSE (
-	GOTO runcode
+	GOTO runcode27
 )
 
-:runcode
-REN python.genome21 python.cmd
+:runcode35
+REN python35.genome21 python.cmd
 COPY python.cmd C:\Windows
-REN python.cmd python.genome21
+REN python.cmd python35.genome21
+GOTO EOF
+
+:runcode27
+REN python27.genome21 python.cmd
+COPY python.cmd C:\Windows
+REN python.cmd python27.genome21
+GOTO EOF
 
 :EOF
